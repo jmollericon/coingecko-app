ab4cf67406d933475e4e1f2821dc9bb6b87da1c6
Jorge Mollericon <jlmollericon@gmail.com>
2021-07-07 18:57:34 -0400

    project ended

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { View, Text, FlatList, StyleSheet } from 'react-native'
+import { View, Text, FlatList, StyleSheet, TextInput, StatusBar } from 'react-native'
+import CoinItem from './components/CoinItem'
 
 const App = () => {
   const [coins, setCoins] = useState([])
+  const [search, setSearch] = useState('')
+  const [refreshing, setRefreshing] = useState(false)
 
   const loadData = async () => {
     const res = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
@@ -15,9 +18,28 @@ const App = () => {
   })
   return (
     <View style={styles.container}>
+        <StatusBar backgroundColor='#141414'/>
+      <View style={styles.header}>
+        <Text style={styles.title}>Crypto Market</Text>
+        <TextInput
+          style={styles.searchInput}
+          placeholder='Search a Coin'
+          placeholderTextColor='#858585'
+          onChangeText={text => setSearch(text)}
+        />
+      </View>
       <FlatList
-        data={coins}
-        renderItem={({ item }) => <Text>{item.name}</Text>}
+        style={styles.list}
+        data={coins.filter(coin => coin.name.toLowerCase().includes(search) || coin.symbol.toLowerCase().includes(search))}
+        renderItem={({ item }) => <CoinItem coin={item} />}
+        showsVerticalScrollIndicator={false}
+        refreshing={refreshing}
+        onRefresh={async () => {
+          console.log('reff')
+          setRefreshing(true)
+          await loadData()
+          setRefreshing(false)
+        }}
       />
     </View>
   )
@@ -28,6 +50,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#141414',
     alignItems: 'center',
     flex: 1
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '90%',
+    marginBottom: 10
+  },
+  title: {
+    color: '#ffffff',
+    marginTop: 10,
+    fontSize: 20
+  },
+  searchInput: {
+    color: '#fff',
+    borderBottomColor: '#4657CE',
+    borderBottomWidth: 1,
+    textAlign: 'center',
+    width: '40%'
+  },
+  list: {
+    width: '90%'
   }
 })
 
