67f7bc0e7dbbf8131214e8b1f302dc4e3bed8def
Jorge Mollericon <jlmollericon@gmail.com>
2021-07-07 18:02:35 -0400

    loadData en use this into TExt

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { View, Text, FlatList, StyleSheet } from 'react-native'
 
 const App = () => {
+  const [coins, setCoins] = useState([])
+
+  const loadData = async () => {
+    const res = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    setCoins(res.data)
+  }
+
+  useEffect(() => {
+    loadData()
+  })
   return (
-    <View>
-      <Text>Hello world!</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={coins}
+        renderItem={({ item }) => <Text>{item.name}</Text>}
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#141414',
+    alignItems: 'center',
+    flex: 1
+  }
+})
 
 export default App
