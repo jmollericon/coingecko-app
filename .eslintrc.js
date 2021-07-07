f10c4ba25dfaffc53348357d4f30abbfb5318abd
Jorge Mollericon <jlmollericon@gmail.com>
2021-07-07 18:55:31 -0400

    update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,19 @@
+const RULES = {
+  OFF: 'off',
+  WARN: 'warn',
+  ERROR: 'error',
+};
 module.exports = {
   env: {
-    es2021: true
+    es2021: true,
   },
   extends: [
+    'plugin:react/recommended',
     'standard'
   ],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  rules: {
-  }
-}
+  rules: {},
+};
